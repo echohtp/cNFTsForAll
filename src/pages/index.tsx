d1720c49e4350e01651fcd66c0a0d9608363566b
Jason Schorr <jason@Jasons-MacBook-Pro-2.local>
2023-08-22 11:18:28 -0400

    add GPS location support

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,43 @@
 import { redirect } from 'next/navigation';
 import { NextPageContext } from 'next'
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
  
+
+// This mints a new NFT to a NEW Tiplink Wallet and sends it to the Page object
 Page.getInitialProps = async (ctx: NextPageContext) => {
   const url = await fetch('http://localhost:3000/api/action')
   return { tiplink: await url.json() }
 }
- 
+
+
 export default function Page({ tiplink }: { tiplink: any }) {
+
+  // Wait 5 seconds for TIPLINK to setup before redirecting the user
   useEffect(()=>{
-    window.location.href = tiplink.url
+    setTimeout(()=>{
+      window.location.href = tiplink.url
+    },5000)
+    
   },[])
-  
-  
+
+  // Get the users GPS location
+  useEffect(()=> {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      setLat(position.coords.latitude)
+      setLon(position.coords.longitude)
+    });
+  })
+
+  // GPS State
+  const [lat, setLat] = useState<any>()
+  const [lon, setLon] = useState<any>()
+
+  return (
+  <>
+    <p>Redirecting page</p>
+    <p>Lat: {lat}</p>
+    <p>Lon: {lon}</p>
+  </>
+  )
+
 }

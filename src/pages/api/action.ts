65a838e7b4026e6fd705bb302a8dbae282c14f41
Jason Schorr <jason@Jasons-MacBook-Pro-2.local>
2023-10-30 14:52:50 +0100

    fix name length

--- src/pages/api/action.ts
+++ src/pages/api/action.ts
@@ -11,28 +11,28 @@ type Data = {
 // Fill this array with (nft) objects
 const _nfts = [
   {
-    name: "I met banana at breakpoint 2023",
+    name: "I met banana",
     description: "This POAP commemorates the delightful encounter with the one and only Banana. A meeting filled with zest, humor, and unforgettable memories, it's a token to cherish our unique connection.",
     symbol: "",
     image: `https://bafybeia5ds3v7n2x76yqjhfjzxyv6fnyb6reavzlmq4wnzsbqor5tgpgta.ipfs.nftstorage.link/1.png`,
     receiverAddress: ""
   },
   {
-    name: "I met banana at breakpoint 2023",
+    name: "I met banana",
     description: "This POAP commemorates the delightful encounter with the one and only Banana. A meeting filled with zest, humor, and unforgettable memories, it's a token to cherish our unique connection.",
     symbol: "",
     image: `https://bafybeia5ds3v7n2x76yqjhfjzxyv6fnyb6reavzlmq4wnzsbqor5tgpgta.ipfs.nftstorage.link/2.png`,
     receiverAddress: ""
   },
   {
-    name: "I met banana at breakpoint 2023",
+    name: "I met banana",
     description: "This POAP commemorates the delightful encounter with the one and only Banana. A meeting filled with zest, humor, and unforgettable memories, it's a token to cherish our unique connection.",
     symbol: "",
     image: `https://bafybeia5ds3v7n2x76yqjhfjzxyv6fnyb6reavzlmq4wnzsbqor5tgpgta.ipfs.nftstorage.link/3.png`,
     receiverAddress: ""
   },
   {
-    name: "I met banana at breakpoint 2023",
+    name: "I met banana",
     description: "This POAP commemorates the delightful encounter with the one and only Banana. A meeting filled with zest, humor, and unforgettable memories, it's a token to cherish our unique connection.",
     symbol: "",
     image: `https://bafybeia5ds3v7n2x76yqjhfjzxyv6fnyb6reavzlmq4wnzsbqor5tgpgta.ipfs.nftstorage.link/4.png`,
@@ -49,23 +49,32 @@ export default async function handler(
   const link = await TipLink.create()
 
   // Generate a new nft via underdog to tiplink wallet
-  const config = {
-    headers: { Authorization: `Bearer ${process.env.NEXT_PUBLIC_UNDERDOG_API_KEY!}` }
-  }
+
+  const options = {
+    // method: 'PUT',
+    headers: {
+      accept: 'application/json',
+      'content-type': 'application/json',
+      authorization: `Bearer ${process.env.NEXT_PUBLIC_UNDERDOG_API_KEY!}`
+    }
+  };
+  
 
   let nft = _nfts[Math.floor(Math.random() * _nfts.length)]
   nft.receiverAddress = link.keypair.publicKey.toBase58()
 
   // get total so far so we can number them correctly 
-  const totRes = await axios.get(`${process.env.NEXT_PUBLIC_UNDERDOG_ENDPOINT}/v2/projects/4?page=1&limit=10`, config)
+  const totRes = await axios.get(`${process.env.NEXT_PUBLIC_UNDERDOG_ENDPOINT}/v2/projects/4?page=1&limit=10`, options)
   const totalResults = totRes.data.nfts.totalResults
 
   nft.name += ` #${totalResults}`
 
+  console.log(nft)
+
   console.log(link.url.toString())
 
   console.log("MINTING")
-  await axios.post(`${process.env.NEXT_PUBLIC_UNDERDOG_ENDPOINT}/v2/projects/${process.env.NEXT_PUBLIC_UNDERDOG_PROJECT_ID}/nfts`, nft, config)
+  await axios.post(`${process.env.NEXT_PUBLIC_UNDERDOG_ENDPOINT}/v2/projects/${process.env.NEXT_PUBLIC_UNDERDOG_PROJECT_ID}/nfts`, nft, options)
 
   console.log("DONE")
 

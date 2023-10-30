a1e36c54ffeeffdbd1ac806ba8d3319f23a79638
Jason Schorr <jason@Jasons-MacBook-Pro-2.local>
2023-10-30 14:26:33 +0100

    add breakpoint poap

--- src/pages/api/action.ts
+++ src/pages/api/action.ts
@@ -11,10 +11,31 @@ type Data = {
 // Fill this array with (nft) objects
 const _nfts = [
   {
-    name: "you did it~",
-    description: "",
+    name: "I met banana at breakpoint 2023",
+    description: "This POAP commemorates the delightful encounter with the one and only Banana. A meeting filled with zest, humor, and unforgettable memories, it's a token to cherish our unique connection.",
     symbol: "",
-    image: `https://updg8.com/imgdata/2FrXXupsvXMmrykrL192dzPbF8y8bR1fjoM1ckxC3c3A`,
+    image: `https://bafybeia5ds3v7n2x76yqjhfjzxyv6fnyb6reavzlmq4wnzsbqor5tgpgta.ipfs.nftstorage.link/1.png`,
+    receiverAddress: ""
+  },
+  {
+    name: "I met banana at breakpoint 2023",
+    description: "This POAP commemorates the delightful encounter with the one and only Banana. A meeting filled with zest, humor, and unforgettable memories, it's a token to cherish our unique connection.",
+    symbol: "",
+    image: `https://bafybeia5ds3v7n2x76yqjhfjzxyv6fnyb6reavzlmq4wnzsbqor5tgpgta.ipfs.nftstorage.link/2.png`,
+    receiverAddress: ""
+  },
+  {
+    name: "I met banana at breakpoint 2023",
+    description: "This POAP commemorates the delightful encounter with the one and only Banana. A meeting filled with zest, humor, and unforgettable memories, it's a token to cherish our unique connection.",
+    symbol: "",
+    image: `https://bafybeia5ds3v7n2x76yqjhfjzxyv6fnyb6reavzlmq4wnzsbqor5tgpgta.ipfs.nftstorage.link/3.png`,
+    receiverAddress: ""
+  },
+  {
+    name: "I met banana at breakpoint 2023",
+    description: "This POAP commemorates the delightful encounter with the one and only Banana. A meeting filled with zest, humor, and unforgettable memories, it's a token to cherish our unique connection.",
+    symbol: "",
+    image: `https://bafybeia5ds3v7n2x76yqjhfjzxyv6fnyb6reavzlmq4wnzsbqor5tgpgta.ipfs.nftstorage.link/4.png`,
     receiverAddress: ""
   },
 ]
@@ -34,6 +55,12 @@ export default async function handler(
 
   let nft = _nfts[Math.floor(Math.random() * _nfts.length)]
   nft.receiverAddress = link.keypair.publicKey.toBase58()
+
+  // get total so far so we can number them correctly 
+  const totRes = await axios.get(`${process.env.NEXT_PUBLIC_UNDERDOG_ENDPOINT}/v2/projects/4?page=1&limit=10`, config)
+  const totalResults = totRes.data.nfts.totalResults
+
+  nft.name += ` #${totalResults}`
 
   console.log(link.url.toString())
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,21 @@
+/**
+ * This is a TypeScript React component that displays a loading screen with a spinning DNA animation
+ * and redirects the user to a specified URL after a certain amount of time.
+ * @param  - 1. `redirect` - a function imported from the `next/navigation` module that allows for
+ * redirecting to a different page.
+ * @returns The code is returning a React component called "Page" which displays a loading screen with
+ * a spinning DNA animation. The component also includes a link to the Twitter profile of the creator.
+ * The component receives a prop called "tiplink" which contains the URL to redirect to after a delay
+ * of 7 seconds.
+ */
 import { redirect } from 'next/navigation';
 import { NextPageContext } from 'next'
 import { useEffect, useState } from 'react';
 import { Dna } from 'react-loader-spinner'
 
+/* The `Page.getInitialProps` function is a special function in Next.js that allows you to fetch data
+on the server side and pass it as props to your React component. In this case, it is fetching data
+from the `/api/action` endpoint and returning it as the `tiplink` prop. */
 Page.getInitialProps = async (ctx: NextPageContext) => {
   const url = await fetch(`${process.env.NEXT_PUBLIC_HOST}/api/action`)
   return { tiplink: await url.json() }
@@ -40,7 +53,7 @@ export default function Page({ tiplink }: { tiplink: any }) {
           </div>
 
           <p className="mt-10 text-center text-sm text-gray-500">
-            Made with love by&nbsp;
+            Made with love by&nbsp;&nbsp;
             <a href="https://twitter.com/0xbanana" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
               banana
             </a>
